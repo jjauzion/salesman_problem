6342e8d1f2bc5f3a36253d2f1ecfe34563fb0b0f
jjauzion <jeremy.jauzion@gmail.com>
2018-07-31 16:43:33 +0200

    salesamn pb dev ongoing

--- main.py
+++ main.py
@@ -1,12 +1,71 @@
 # -*-coding:Utf-8 -*
 
+import random
+
 from src.city import City
 from src.individual import Individual
 
+def     normalize_fitness(population):
+    worse_fitness = population[0].fitness
+    best_fitness = population[0].fitness
+    for i in population:
+        if i.fitness < best_fitness:
+            best_fitness = i.fitness
+        elif i.fitness > worse_fitness:
+            worse_fitness = i.fitness
+
+
+def     set_individual_breed_probability(population):
+    worse_fitness = population[0].fitness
+    best_fitness = population[0].fitness
+    for i in population:
+        if i.fitness < best_fitness:
+            best_fitness = i.fitness
+        elif i.fitness > worse_fitness:
+            worse_fitness = i.fitness
+    best_fitness = (best_fitness - worse_fitness) * -1
+    for i in population:
+        i.adjusted_fitness = (i.fitness - worse_fitness) * -1
+        i.breed_proba = i.adjusted_fitness * 100 / best_fitness
+
+def     get_parents(population):
+    parent = []
+    while len(parent) < 2:
+        element = random.choice(population)
+        if element.breed_proba >= random.randrange(100):
+            parent.append(element)
+    return (parent[0], parent[1])
+
+def     next_generation(population, population_size):
+    next_gen = []
+    i = 0
+    while i < population_size:
+        father, mother = get_parents(population)
+        print("father = ", father)
+        print("mother = ", mother)
+        next_gen.append(Individual(parent_a=mother, parent_b=father))
+        i += 1
+    return next_gen
+
+nb_of_city = 10
+population_size = 10
+nb_iteration = 100
+
 city2travel = []
-nb_of_city = 3
 for i in range(nb_of_city):
     city2travel.append(City(5))
-
-new = Individual(city_list=city2travel)
-print(new)
+population = []
+for i in range(population_size):
+    population.append(Individual(city_list=city2travel))
+set_individual_breed_probability(population)
+for i in population:
+    print(i, " ; proba = ", i.breed_proba)
+average_fitness = [sum(i.fitness for i in population) / population_size]
+'''
+average_fitness = [sum(i.fitness for i in population) / population_size]
+for i in range(nb_iteration):
+    population = next_generation(population, population_size)
+    set_individual_breed_probability(population)
+    average_fitness.append(sum(i.fitness for i in population) / population_size)
+    print("Population num {}: av fitness = {}".format(i, average_fitness[i]))
+'''
--- src/individual.py
+++ src/individual.py
@@ -26,11 +26,10 @@ class       Individual():
         self.dna += parent_b.dna[parent_a.size // 2:]
 
     def     set_fitness(self):
+        """Compute the fitness of the individual. The lower, the better"""
         self.fitness = 0
         for i in range(self.size - 1):
-            print(get_distance(self.dna[i], self.dna[i + 1]))
             self.fitness += get_distance(self.dna[i], self.dna[i + 1])
-            print(self.fitness)
 
     def     __init__(self, city_list=None, parent_a=None, parent_b=None):
         """Constructor of Individual class
@@ -42,11 +41,21 @@ class       Individual():
             self.dna = city_list
             random.shuffle(self.dna)
         elif parent_a and parent_b:
-            self.dna = self.breed(parent_a, parent_b)
+            self.breed(parent_a, parent_b)
         else:
-            raise ValueError("Error in Indivdual creation: missing parents or city list")
+            raise ValueError(
+                    "Error in Indivdual creation: missing parents or city list")
         self.size = len(self.dna)
         self.set_fitness()
+        self._str = None
+        self.adjusted_fitness = 0
+        self.breed_proba = 0
 
     def     __repr__(self):
-        return "DNA : {}\nfitness = {}".format(self.dna, self.fitness)
+        if not self._str:
+            self._str = "DNA:"
+            for i in range(self.size):
+                self._str += " {}".format(self.dna[i].id)
+            #self._str += "\nfitness: {}".format(self.fitness)
+            #self._str += "\nbreed proba: {}".format(self.breed_proba)
+        return self._str
